6f411b16eef2834865562b09b33af7750837cbea
EwelinaZarnowska <101662525+EwelinaZarnowska@users.noreply.github.com>
2022-10-03 18:15:51 +0200

    Update Main.java

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -1,35 +1,38 @@
 package org.example;
-
+ 
 import java.util.Random;
 import java.util.Scanner;
-
+ 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(game());
+    }
+ 
+    public static String game() {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        int numberComputer = random.nextInt(100);
+        int randomNumber = random.nextInt(100);
         System.out.println("Guess the number from 0 to 99");
-
-        int numberUser;
-        int numberTrials = 0;
-        int numberTrials2 = 5;
-        do {
-            numberTrials++;
-            numberTrials2--;
-            System.out.print("Enter number: ");
-            numberUser = scanner.nextInt();
-
-            if (numberUser < numberComputer) {
-                System.out.println("Your number is GREATER than the one you are trying to guess. You have " + numberTrials2 + " more tries.");
-        }   else if (numberUser > numberComputer) {
-                System.out.println("Your number is LOWER than the one you are trying to guess. You have " + numberTrials2 + " more tries.");
+        for (int i = 5; i > 0; i--) {
+            int tires = i - 1;
+            int userGuess = scanner.nextInt();
+            if (randomNumber == userGuess) {
+                return "You guessed it";
+            } else if (randomNumber > userGuess) {
+                System.out.println("Your number is GREATER than the one you are trying to guess");
+                System.out.println("You have " + tires + " more tries.");
+                if (tires > 0) {
+                    System.out.println("Please try again");
+                }
+ 
             } else {
-                System.out.println("You guessed it!");
+                System.out.println("Your number is LOWER than the one you are trying to guess");
+                System.out.println("You have " + tires + " more tries.");
+                if (tires > 0) {
+                    System.out.println("Please try again");
+                }
             }
-        } while (numberTrials < 5); System.out.println("Sorry you didn't guess the number, the answer was: " + numberComputer);
-
+        }
+        return "Sorry you didn't guess the number, the answer was: " + randomNumber;
     }
-
-
 }
-
